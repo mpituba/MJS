541058517884393445f761e13ff051dca9d06c3f
Marcelo Pituba <mpituba@yahoo.com>
2022-06-01 10:08:06 -0300

    Funções puras - programação funcional

--- 02-07-Programacao-funcional-vs-imperativa/index.js
+++ 02-07-Programacao-funcional-vs-imperativa/index.js
@@ -3,9 +3,17 @@ var person = {
   name: 'Lucas'
 }
 
+//Função pura - Não altera dado do objeto
 function getRemainingYearsToMajorty(person) {
   return 18 - person.age
 }
 
+//Função impura gera efeitos pois altera o estado do atributo
+function increasePersonAge (person) {
+  person.age = person.age + 1
+}
+
+increasePersonAge(person)
 var remainingYears = getRemainingYearsToMajorty(person)
 console.log(remainingYears)
+
