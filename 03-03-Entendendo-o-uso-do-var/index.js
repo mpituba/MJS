e32266616e0c0cd0454e34b3ed7273a155723438
Marcelo Pituba <mpituba@yahoo.com>
2022-06-01 12:08:44 -0300

    Entendo o var

---
+++ 03-03-Entendendo-o-uso-do-var/index.js
@@ -0,0 +1,14 @@
+/*
+Esta aula explica como uma declaração var fica restrita
+a uma função, sendo local a ela.
+Sendo dirente de uma redeclaração de valor.
+*/
+const lenght = 'uma string qualquer'.length
+
+console.log(lenght)
+
+var age = 21
+
+var age = 22
+
+
