38e80caf9644db9e0a2969d042b78fee676f5528
Marcelo Pituba <mpituba@yahoo.com>
2022-06-01 10:48:37 -0300

    03-01- Tipos primitivos, Arrays e Objetos

---
+++ 03-01-Relembrando alguns tópicos/index.js
@@ -0,0 +1,27 @@
+/*
+Tipos primitivos
+
+String - cDEIA DE CARACTERES
+Number - Representa qualquer número
+Boolean - true ou false
+Undefine - Variável declarada não definida.
+Null - Valor definido como nulo
+
+Arrays
+
+É uma lista de dados - armazena dados de todos os tipos em
+um "grupo", pode ser comparado com uma lista.
+É Iterável - podemos percorrer item por item e executar
+algum bloco de código.
+Estreturado por índice e valor
+
+Objeto
+
+Representa uma unidade; uma pessoa, um formulário, um carro,
+etc.
+Estruturado por chave e valor
+nome  : daniel
+chave   valor
+Possui propriedades que são chaves e valor.
+*/
+
