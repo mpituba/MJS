463908661595e41f80eaacb1946d3cf7a7aeac31
Marcelo Pituba <mpituba@yahoo.com>
2022-06-06 15:59:15 -0300

    Introdução às Promises

---
+++ 07-01-Introducao-as-promises/index.js
@@ -0,0 +1,13 @@
+/*
+Definição de Assincronismo: Código que é 
+executado sem bloquear o código principal.
+
+promises: São objetos que representam um
+valor que pode estar disponível, agora,
+depois ou nuca.
+
+Uma promise pode ser resolvida ou rejeitada.
+
+Pode ter três status: pendente, resolvida,
+ou rejeitada.
+*/
