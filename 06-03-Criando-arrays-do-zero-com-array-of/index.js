98fe3c62934afeae1eb1a26e23e3c9a5a9b0fe0c
Marcelo Pituba <mpituba@yahoo.com>
2022-06-06 13:32:27 -0300

    Criando arrays com array.of e new Array

---
+++ 06-03-Criando-arrays-do-zero-com-array-of/index.js
@@ -0,0 +1,10 @@
+//Cria um array de 6 posições indefinidas ou vazias
+const array = new Array(6)
+console.log(array)
+
+//Cria um array de uma posição com o valor 6
+const array2 = Array.of(6)
+console.log(array2)
+
+const array3 = Array.of(1, 2, 3, 4, 5, 6)
+console.log(array3)
