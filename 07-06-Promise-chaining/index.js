6a7e8fbd91de836907208125809e73e595e25d7a
Marcelo Pituba <mpituba@yahoo.com>
2022-06-07 10:57:41 -0300

    Promises em paralelo

--- 07-06-Promise-chaining/index.js
+++ 07-06-Promise-chaining/index.js
@@ -1,6 +1,17 @@
 /* Código não executou! Verificar endpoints! Talvez rodar
-    num browser...*/
-    
+    num browser...
+
 fetch('https://jsonplaceholder.typicode.com/todos/1')
     .then(res => res.json())
     .then(data => console.log(data))
+*/
+
+/*
+
+Link mdn sobre o assunto:
+https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Using_promises#encadeamento
+
+*/
+
+
+
